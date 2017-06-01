9918bbc8c32d4a5a6decdc642b56103970c963d2
Brian Telintelo <brian@madebyatomicrobot.com>
2017-06-01 14:38:31 -0400

    fixed merge

--- Sources/Player.swift
+++ Sources/Player.swift
@@ -282,7 +282,6 @@ open class Player: UIViewController {
     internal var _avplayer: AVPlayer
     internal var _playerItem: AVPlayerItem?
     internal var _playerView: PlayerView!
-    internal var _timeObserver: Any!
     internal var _timeObserver: Any?
     
     internal var _playerView: PlayerView = PlayerView(frame: .zero)
